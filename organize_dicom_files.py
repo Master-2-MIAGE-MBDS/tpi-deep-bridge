0df7b37033d4ce006577f8bcfd379d98083bb1eb
Adams Pierre David <57180807+adamspd@users.noreply.github.com>
2023-03-22 20:53:18 +0100

    trier les fichiers dicom

--- organize_dicom_files.py
+++ organize_dicom_files.py
@@ -3,8 +3,9 @@ import sys
 import pydicom
 import logging
 import shutil
+from collections import defaultdict
 
-patient_id_to_folder = {}
+patient_series_to_folder = defaultdict(lambda: defaultdict(str))
 
 
 def create_directory(path):
@@ -19,45 +20,51 @@ def move_file(source, destination):
         shutil.move(source, destination)
 
 
-def read_dicom_metadata(filename):
+def read_dicom_metadata(filename, output_folder):
     try:
         ds = pydicom.dcmread(filename, stop_before_pixels=True)
         if ds.Modality != 'CT':
             logging.warning('File {} is not a CT scan'.format(filename))
         else:
             patient_id = ds.PatientID
-            if patient_id not in patient_id_to_folder:
+            series_uid = ds.SeriesInstanceUID
+            series_folder = os.path.join(output_folder, patient_id, series_uid)
+            if series_uid not in patient_series_to_folder[patient_id]:
                 logging.info('Found new patient: {}'.format(patient_id))
-                patient_id_to_folder[patient_id] = os.path.join('data', patient_id)
-                create_directory(patient_id_to_folder[patient_id])
-            move_file(filename, os.path.join(patient_id_to_folder[patient_id], os.path.basename(filename)))
+                logging.info('Found new series: {}'.format(series_uid))
+                patient_series_to_folder[patient_id][series_uid] = series_folder
+                create_directory(series_folder)
+            move_file(filename, os.path.join(series_folder, os.path.basename(filename)))
     except pydicom.errors.InvalidDicomError as e:
-        invalid_dicom_folder = os.path.join('data', 'invalid_dicom_files')
+        invalid_dicom_folder = os.path.join(output_folder, 'invalid_dicom_files')
         create_directory(invalid_dicom_folder)
         move_file(filename, os.path.join(invalid_dicom_folder, os.path.basename(filename)))
+    except FileNotFoundError:
+        pass
 
 
-def load_scan(path):
+def load_scan(path, output_folder):
     print('glob: {}'.format(path))
     for root, dirs, filenames in os.walk(path):
         for filename in filenames:
             if filename.endswith('.dcm'):
                 filepath = os.path.join(root, filename)
-                read_dicom_metadata(filepath)
+                read_dicom_metadata(filepath, output_folder)
             else:
                 if filename.endswith('.cab'):
                     os.remove(os.path.join(root, filename))
-                logging.warning('File {} is not a dicom file'.format(filename))
+                else:
+                    logging.warning('File {} is not a dicom file'.format(filename))
     logging.info('Finished reading dicom files')
 
 
-def main(source_folder):
-    create_directory('data')
-    load_scan(source_folder)
+def main(source_folder, output_folder):
+    create_directory(output_folder)
+    load_scan(source_folder, output_folder)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python organize_dicom_files.py <source_folder>")
+    if len(sys.argv) != 3:
+        print("Usage: python organize_dicom_files.py <source_folder> <output_folder>")
         sys.exit(1)
-    main(sys.argv[1])
+    main(sys.argv[1], sys.argv[2])
